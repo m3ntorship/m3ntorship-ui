1df2c02c3eb53460fcd000f984011281cb7fbdd4
MAlngaawy <m.alngaawy@gmail.com>
2022-06-13 10:52:48 +0200

    add one more template

--- src/example/ExComponent.tsx
+++ src/example/ExComponent.tsx
@@ -3,7 +3,7 @@ import { ExTypes } from './type';
 import cn from 'classnames';
 
 const ExComponent = ({content, styles, type}: ExTypes) => {
-  return <div className={cn(styles, " p-4 ",{'bg-black text-white' : type === 'primary', 'bg-test2 text-black' : type === 'secondary' })} > {content} Test Component The Type is {type} </div>
+  return <div className={cn(styles, " p-4",{'bg-test text-white' : type === 'primary', 'bg-test2 text-black' : type === 'secondary' })} > {content} Test Component The Type is {type} </div>
 }
 
 export {ExComponent}
--- stories/ExComponent.stories.tsx
+++ stories/ExComponent.stories.tsx
@@ -11,9 +11,12 @@ export default {
 const Template : Story<typeof ExComponent> = (args) => <ExComponent {...args} />
 
 export const Default = Template.bind({})
-Default.args = {styles: "bg-gray-500 p-4", content : "Defalt"}
+Default.args = {styles: "bg-gray-500", content : "Defalt"}
 
 export const Primary = Template.bind({})
 Primary.args = {styles: "", content: "Primary", type: "primary"}
+
+export const Secondary = Template.bind({})
+Secondary.args = {styles: "", content: "Primary", type: "secondary"}
 
 // export const Primary: Story<typeof ExComponent> = () => <ExComponent type="primary" styles="bg-test p-4" content='Here is the content' />
