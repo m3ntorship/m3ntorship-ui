ea43515f7538b854166fce49e9440f911b592e68
MAlgaawy <m.alngaawy@gmail.com>
2022-06-13 21:37:48 +0200

    handle access the stories in the src folder

--- src/example/ExComponent.stories.tsx
+++ src/example/ExComponent.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { ExComponent } from '../src/example/ExComponent';
+import { ExComponent } from './ExComponent';
 
 export default {
   title: 'example component',
@@ -15,5 +15,3 @@ Default.args = {styles: "bg-gray-500 p-4", content : "Defalt"}
 
 export const Primary = Template.bind({})
 Primary.args = {styles: "", content: "Primary", type: "primary"}
-
-// export const Primary: Story<typeof ExComponent> = () => <ExComponent type="primary" styles="bg-test p-4" content='Here is the content' />
