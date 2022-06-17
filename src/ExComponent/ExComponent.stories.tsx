736b029d2f38f6d57d98b0a60777ec276e226f2e
MAlngaawy <m.alngaawy@gmail.com>
2022-06-17 12:40:56 +0200

    feat: create the compoonent

---
+++ src/ExComponent/ExComponent.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+
+import { ExComponent } from './ExComponent';
+
+export default {
+  title: 'example component',
+  component: ExComponent,
+} as Meta<typeof ExComponent>;
+
+const Template: Story<typeof ExComponent> = (args) => <ExComponent {...args} />;
+
+export const Default = Template.bind({});
+Default.args = { styles: '', content: 'Defalt' };
+
+export const Primary = Template.bind({});
+Primary.args = { styles: '', content: 'Primary', type: 'primary' };
+
+export const Secondary = Template.bind({});
+Secondary.args = { styles: '', content: 'Primary', type: 'secondary' };
