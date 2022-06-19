662c5813ffc14ba87f27caa2e9844220cd328f53
MAlngaawy <m.alngaawy@gmail.com>
2022-06-19 21:27:18 +0200

    feat: start create the filled type

--- src/ButtonComponent/Button.tsx
+++ src/ButtonComponent/Button.tsx
@@ -1,14 +1,36 @@
-import cn from 'classnames';
+import classnames from 'classnames';
 import React from 'react';
 
 import { ButtonProps } from './types';
 
-const Button = ({ content }: ButtonProps): JSX.Element => {
-  return (
-    <div>
-      <button className={cn(`py-med px-lrg rounded-med`)}>{content}</button>
-    </div>
-  );
+const Button = ({
+  content,
+  variant,
+  color,
+  size,
+}: ButtonProps): JSX.Element => {
+  if (variant === 'filled') {
+    return (
+      <button
+        className={classnames('rounded-med', {
+          // handle the sizes
+          ' py-med px-lrg text-20 ': size === 'large',
+          ' py-xs px-med text-base': size === 'medium',
+          ' py-xs px-sm text-14': size === 'small',
+
+          // handle the color
+          'bg-primary hover:bg-primary-hover text-white ': color === 'primary',
+          'bg-secondary hover:bg-secondary-hover text-white ':
+            color === 'secondary',
+          'bg-neutral hover:bg-neutral-hover text-white ': color === 'neutral',
+        })}
+      >
+        {content}
+      </button>
+    );
+  } else {
+    return <button>Test</button>;
+  }
 };
 
 export { Button };
