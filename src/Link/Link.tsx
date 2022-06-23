8f83b7f8e322d344c0754a30dd1e5489093e1cfa
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2022-06-23 16:54:40 +0200

    feat: add Link component

---
+++ src/Link/Link.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames';
+import React, { FC } from 'react';
+
+import { LinkProps } from './Link.types';
+
+/** The Link component is an element for navigation. */
+const Link: FC<LinkProps> = ({
+  className,
+  size,
+  children,
+  disabled,
+  href,
+  ...props
+}) => {
+  return (
+    <a
+      className={classNames(
+        'font-inter text-primary-text font-regular px-xs tracking-normal hover:bg-action-background-hover visited:font-semi-bold',
+        {
+          'text-base leading-24': size !== 'small',
+          'text-14 leading-20': size === 'small',
+          'text-disabled-text hover:bg-background-surface pointer-events-none cursor-not-allowed':
+            disabled,
+        },
+        className
+      )}
+      href={disabled || !href ? '/' : href}
+      {...props}
+    >
+      {children}
+    </a>
+  );
+};
+
+export { Link };
---
+++ src/Link/Link.types.ts
@@ -0,0 +1,11 @@
+import { AnchorHTMLAttributes, ReactNode } from 'react';
+export interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  /**Specifies the URL of the page the link goes to.*/
+  href?: string;
+  /**Changes the size of the Link.*/
+  size?: 'small';
+  /**The content of the component*/
+  children?: ReactNode;
+  /**Transforms the Link to Disabled mode*/
+  disabled?: boolean;
+}
