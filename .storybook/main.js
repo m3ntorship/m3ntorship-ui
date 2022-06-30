d114d6f1fe000bf47a7b3000dd08e22501ea1b01
atwfk <ahmed.twfiek8@gmail.com>
2022-06-30 07:45:39 +0200

    config: load svg for storybook with svgr

--- .storybook/main.js
+++ .storybook/main.js
@@ -20,4 +20,19 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  webpackFinal: async config => {
+    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+    const fileLoaderRule = config.module.rules.find(
+      rule => rule.test && rule.test.test('.svg')
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config;
+  },
 };
