62e8e12f0e61d18fc92780af67e3f69e0b2e6884
MAlngaawy <m.alngaawy@gmail.com>
2022-07-03 19:53:51 +0200

    feat: add another test for disabled button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,8 +4,14 @@ import * as renderer from 'react-test-renderer';
 import { Button } from './index';
 
 describe('Button', () => {
-  it('should render button component', () => {
+  it('should render button component with {Default inner text, and className = {shared variant-filled-shared size-large variant-filled-primary}}', () => {
     const tree = renderer.create(<Button />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('should return ghost disabled button', () => {
+    const tree = renderer.create(
+      <Button content="secondary disabled" variant="ghost" disabled={true} />
+    );
     expect(tree).toMatchSnapshot();
   });
 });
