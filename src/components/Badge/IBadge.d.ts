f2c46e9e21c651585d17041f9fa0eeff379a4447
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2022-10-04 16:49:08 +0200

    Fix Badge component type error & Added new size to Avatar component  (#91)
    * feat: added a new size for the avatar compoennt (#90)
    
    Co-authored-by: abdulrahman <abd.elsayed@hotmail>
    
    * fix: use text prop to disaplay the text instead of using children prop (#89)
    
    Co-authored-by: abdulrahman <abd.elsayed@hotmail>
    
    Co-authored-by: abdulrahman <abd.elsayed@hotmail>

--- src/components/Badge/IBadge.d.ts
+++ src/components/Badge/IBadge.d.ts
@@ -1,6 +1,6 @@
 export interface IBadgeProps {
   /**Add The text or component which will be wrapped by the Badge component */
-  children?: string;
+  text?: string;
   /**Choose which color variant  for the Badge component*/
   color?: 'primary' | 'secondary' | 'warning' | 'info' | 'error' | 'success';
   /**add additional classes for Badge component */
